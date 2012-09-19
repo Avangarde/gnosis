8dacd5543c5a514f66214b2fa8f556ebdea40188
Zergio <Zergio@Zergio-Beast>
2012-09-19 15:38:42 -0500

    Agregado StudentVO unicamente

---
+++ src/main/java/org/avangarde/gnosis/vo/SubjectVo.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.avangarde.gnosis.vo;
+
+/**
+ *
+ * @author Zergio
+ */
+class SubjectVo {
+    
+}
