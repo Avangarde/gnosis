f4d4ec1d97a605cbedc24eef428aa7f50d7b1afa
Familia Martinez <Familia Martinez@martinez.Familia>
2012-09-20 18:53:39 -0500

    Añadidos los ServiceFactory's restantes

--- src/main/java/org/avangarde/gnosis/businesslogic/service/ServiceFactory.java
+++ src/main/java/org/avangarde/gnosis/businesslogic/service/ServiceFactory.java
@@ -9,9 +9,50 @@ public class ServiceFactory {
     private ServiceFactory() {
     }
 
+    public ActivityService getActivityService() {
+        return ActivityService.getInstance();
+    }
+    
+    public CommentService getCommentService() {
+        return CommentService.getInstance();
+    }
+    
+    public EventService getEventService() {
+        return EventService.getInstance();
+    }
+    
+    public ProgramService getProgramService() {
+        return ProgramService.getInstance();
+    }
+    
+    public PublicationService getPublicationService() {
+        return PublicationService.getInstance();
+    }
+    
     public StudentService getStudentService() {
         return StudentService.getInstance();
     }
+    
+    public StudygroupService getStudygroupService() {
+        return StudygroupService.getInstance();
+    }
+    
+    public SubjectService getSubjectService() {
+        return SubjectService.getInstance();
+    }
+    
+    public TopicService getTopicService() {
+        return TopicService.getInstance();
+    }
+    
+    public TutorService getTutorService() {
+        return TutorService.getInstance();
+    }
+    
+    public TutorSubjectService getTutorSubjectService() {
+        return TutorSubjectService.getInstance();
+    }
+    
     private static ServiceFactory instance;
 
     public static synchronized ServiceFactory getInstance() {
