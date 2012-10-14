7decfa181509c1f95a1e69b809fc4b6481bfa36f
Jeisson Andrés Vergara Vargas <javergarav@unal.edu.co>
2012-10-14 05:51:21 -0500

    Agregado ResourceBean.

---
+++ src/main/java/org/avangarde/gnosis/presentation/controller/ResourceBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.avangarde.gnosis.presentation.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ApplicationScoped;
+
+/**
+ *
+ * @author andres
+ */
+@ManagedBean
+@ApplicationScoped
+public class ResourceBean {
+
+    /**
+     * Creates a new instance of ResourceBean
+     */
+    public ResourceBean() {
+    }
+}
