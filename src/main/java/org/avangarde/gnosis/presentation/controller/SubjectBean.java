def0dcfd0062f30c51feb42be134ad6937f38dc2
zergio <zergio@zergio-Lupin>
2012-11-11 16:21:52 -0500

    Merge de las cosas que no se mezclaron bien, pendiente  testeo y piulimiento de codigo...
    Jasjd<

--- src/main/java/org/avangarde/gnosis/presentation/controller/SubjectBean.java
+++ src/main/java/org/avangarde/gnosis/presentation/controller/SubjectBean.java
@@ -1,13 +1,20 @@
 package org.avangarde.gnosis.presentation.controller;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import org.avangarde.gnosis.businesslogic.facade.FacadeFactory;
+import org.avangarde.gnosis.businesslogic.facade.StudentFacade;
+import org.avangarde.gnosis.businesslogic.facade.SubjectFacade;
+import org.avangarde.gnosis.businesslogic.facade.TutorFacade;
+import org.avangarde.gnosis.businesslogic.facade.TutorSubjectFacade;
 import org.avangarde.gnosis.vo.SubjectVo;
+import org.avangarde.gnosis.vo.TutorSubjectVo;
+import org.avangarde.gnosis.vo.TutorVo;
 
 /**
  *
@@ -106,5 +113,90 @@ public class SubjectBean implements Serializable {
             setDescription(subject.getDescription());
             setNumGroups(subject.getNumGroups());
         }
+    }
+    
+    
+    public String becomeTutorOnSubject() {
+        
+        //TODO revisar cuando no se esta suscrito a la materia
+
+
+        //Logique
+        //Si usuario actual aun no es tutor haz:
+        //Crear nuevo tutor a partir de la información del usuario
+
+        if (user.isLoggedIn()) {
+
+            StudentFacade studentFacade = FacadeFactory.getInstance().getStudentFacade();
+
+            TutorVo tutorVo = new TutorVo();
+
+            tutorVo.setId(user.getId());
+            tutorVo.setStudentId(user.getId());
+
+            if (!studentFacade.isTutor(tutorVo)) {
+
+                becomeTutor(tutorVo);
+
+            }
+
+            //continuando
+
+
+            SubjectFacade subjectFacade = FacadeFactory.getInstance().getSubjectFacade();
+            int subjectCode = code;
+
+            SubjectVo subjectVo = subjectFacade.find(code);
+
+            List<TutorSubjectVo> tutorList = subjectVo.getTutorSubjectList();
+
+            TutorSubjectVo tutorSubjectVo = new TutorSubjectVo();
+
+            tutorSubjectVo.setSubjectCode(subjectCode);
+            tutorSubjectVo.setReputation(0);
+            tutorSubjectVo.setTutorId(tutorVo.getId());
+
+            TutorSubjectFacade tutorSubjectFacade = FacadeFactory.getInstance().getTutorSubjectFacade();
+            
+            tutorSubjectFacade.create(tutorSubjectVo);
+
+//            tutorList.add(tutorSubjectVo);
+//            subjectVo.setTutorSubjectList(tutorList);
+//            boolean flag = subjectFacade.update(subjectVo);
+
+            //Bean de tutor
+
+        }
+
+
+        //Luego haz:
+        //Obtener lista de tutores de la materia
+        //agrega al tutor
+
+        //clausura de cosas
+        return "success";
+
+    }
+
+    public void becomeTutor(TutorVo tutorVo) {
+        TutorFacade tutorFacade = FacadeFactory.getInstance().getTutorFacade();
+
+        //retorna un tutor vacio, que se debe crear antes de continuar
+        //rellenar
+        tutorVo.setStudentId(tutorVo.getId());
+
+        //defecto
+        int def = 0;
+        tutorVo.setNumberStudents(def);
+        tutorVo.setNumberVotes(def);
+        tutorVo.setPublishedResources(def);
+        tutorVo.setQuestionReceived(def);
+        tutorVo.setReputation(def);
+
+        tutorVo.setUserName(user.getUserName());
+
+
+        tutorFacade.create(tutorVo);
+
     }
 }
